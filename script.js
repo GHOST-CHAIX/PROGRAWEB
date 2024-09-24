e9dd42c1a271a1bf65b658533d8a496b9818291e
GHOST-CHAIX <lchaixp2100@alumno.ipn.mx>
2024-09-24 17:58:28 -0600

    Subida al repositorio escolar

---
+++ script.js
@@ -0,0 +1,47 @@
+//CLASE 24/09/24
+
+//CHAIX PADILLA LEONARDO
+
+/*
+variables: Las variables guardan pequeña informacion que puedes reutilizar 
+
+
+Declaracion de variables
+
+*/
+
+let nombre = "pedro";
+
+const PI = 3.1416;
+
+var edad = 34;
+
+edad = 23;
+
+console.log(edad);
+
+console.log(PI);
+
+//OPERADORES
+
+let a = 5;
+
+let b = 4;
+
+let suma = a + b;
+let resta = a - b;
+
+let division = a / b;
+
+let multiplicacion = a * b;
+
+console.log(suma, resta, division, multiplicacion);
+
+//operadores a comparacion
+
+a > b;
+
+a < b;
+a == b;
+
+a === b;
