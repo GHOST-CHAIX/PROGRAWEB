1b060ee3db7cd497e58398c550943f177217efa8
GHOST-CHAIX <lchaixp2100@alumno.ipn.mx>
2024-10-22 17:48:16 -0600

    Clase del 22/10/24 funciones

---
+++ Practica4-22/app.js
@@ -0,0 +1,20 @@
+//Definimos una Funcion Normal
+function hola(a,b){
+    return a+b;
+}
+console.log(hola(5,2));
+
+//? Convertir en funcion flecha 
+
+const suma = (a,b) => {
+    return a+b;
+}
+console.log(suma(56,3));
+
+//Simplificar 
+const mota = (a,b) => a+b;
+console.log(mota(80,100));
+
+//!Solo un parametro
+const cuadrado = x => x*x;
+console.log(cuadrado(8));
